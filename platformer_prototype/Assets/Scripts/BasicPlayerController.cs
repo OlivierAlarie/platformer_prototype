f75b1f7672be4e1742336de57c6318845c2669f4
etiennecaya <etiennecaya@gmail.com>
2022-02-07 22:16:44 -0500

    Initial Jump + Movement of Player Character

--- platformer_prototype/Assets/Scripts/BasicPlayerController.cs
+++ platformer_prototype/Assets/Scripts/BasicPlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,30 +6,36 @@ using UnityEngine;
 public class BasicPlayerController : MonoBehaviour
 {
     [Header("Camera")]
-    [SerializeField]
-    private Camera _mainCamera;
-    [SerializeField]
-    private Transform _cameraRoot;
-    [SerializeField]
-    private float _cameraSensitivityX = 360;
-    [SerializeField]
-    private float _cameraSensitivityY = 360;
-    [SerializeField]
-    private float _maxDownwardAngle = 20;
-    [SerializeField]
-    private float _maxUpwardAngle = -60;
-
+    public Camera _mainCamera;
+    public Transform _cameraRoot;
+    public float _cameraSensitivityX = 360;
+    public float _cameraSensitivityY = 360;
+    public float _maxDownwardAngle = 20;
+    public float _maxUpwardAngle = -60;
 
     private float _targetRotationH = 0;
     private float _targetRotationV = 0;
 
     [Header("Character")]
-    [SerializeField]
-    private CharacterController _characterController;
-    [SerializeField]
-    private float _playerSpeed = 5;
+    public CharacterController _characterController;
+    public float _playerMaxSpeed = 5f;
+    public float _playerJumpSpeed = 5f;
+    public float _playerJumpLeniency = 0f;
+    public float _playerJumpGravMult = 1f;
+    public float _gravity = -9.81f;
+    public float _terminalVelocityY = -53f;
+    public Transform _rootGeometry;
 
-    private Vector3 _targetMotion;
+    private Vector3 _targetDirection = Vector3.zero;
+    private float _targetVelocityY = 0f;
+    private bool _playerJumping = false;
+
+    private enum PlayerStates 
+    {
+        Idle,
+        Walk,
+        Jump
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -41,17 +48,37 @@ public class BasicPlayerController : MonoBehaviour
     {
         InputsCheck();
         CameraCheck();
+        GravityCheck();
         MovePlayer();
     }
 
     void InputsCheck()
     {
         //Horizontal Movement
-        _targetMotion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _targetDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
         //Vertical Movement
-        if(_characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if(_characterController.isGrounded)
+        {
+            _playerJumping = false;
+            _playerMaxSpeed = 5f;
+            _playerJumpGravMult = 1f;
+            if (_playerJumpLeniency > 0 || Input.GetKeyDown(KeyCode.Space))
+            {
+                _targetVelocityY = _playerJumpSpeed;
+                _playerJumping = true;
+            }
+        }
+        else
         {
 
+            if (Input.GetKeyUp(KeyCode.Space) && _playerJumping || _targetVelocityY <= 0)
+            {
+                _playerJumpGravMult = 2f;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+            }
         }
     }
 
@@ -68,11 +95,29 @@ public class BasicPlayerController : MonoBehaviour
         _cameraRoot.transform.rotation = Quaternion.Euler(_targetRotationV, _targetRotationH, 0.0f);
     }
 
+    void GravityCheck()
+    {
+        if (!_characterController.isGrounded)
+        {
+            _targetVelocityY += _gravity * _playerJumpGravMult * Time.deltaTime;
+            if (_targetVelocityY < _terminalVelocityY)
+            {
+                _targetVelocityY = _terminalVelocityY;
+            }
+        }
+    }
+
     void MovePlayer()
     {
         //Use _cameraRoots rotation, making forward always in front of the camera
-        _targetMotion = Quaternion.Euler(0.0f, _cameraRoot.rotation.eulerAngles.y, 0.0f) * _targetMotion;
+        _targetDirection = Quaternion.Euler(0.0f, _cameraRoot.rotation.eulerAngles.y, 0.0f) * _targetDirection;
+        //assign targetDirection to Geometry ?
+        _rootGeometry.transform.LookAt(_characterController.transform.position + _targetDirection);
 
-        _characterController.Move(_targetMotion * _playerSpeed * Time.deltaTime);
+
+        _targetDirection *= _playerMaxSpeed;
+        _targetDirection.y = _targetVelocityY;
+
+        _characterController.Move(_targetDirection * Time.deltaTime);
     }
 }
