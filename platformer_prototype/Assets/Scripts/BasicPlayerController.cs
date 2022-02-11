1d0f27ee4a4fb8d9b243685c9c08abc4ef5e6dad
etiennecaya <etiennecaya@gmail.com>
2022-02-10 22:09:01 -0500

    Added Coyote Timer + Smoother Speed Up/Speed Down
    Coyote Timer uses the same jump leniency as earlier
    Speed Up/Speed Down uses the same rate

--- platformer_prototype/Assets/Scripts/BasicPlayerController.cs
+++ platformer_prototype/Assets/Scripts/BasicPlayerController.cs
@@ -21,18 +21,24 @@ public class BasicPlayerController : MonoBehaviour
     [Header("Character")]
     public CharacterController _characterController;
     public float MaxSpeed = 5f; //Target speed for the character
+    public float TimeToMaxSpeed = 1f;//Time to reach max speed (in Seconds)
     public float JumpHeight = 1f; //Target height for the character
-    public float JumpLeniency = 0.15f; // Seconds of leniency where a jump attempt is registered before reaching the ground
+    public float JumpLeniency = 0.15f; // Seconds of leniency where a jump attempt is registered before reaching the ground / after leaving the ground without jumping
     public float Gravity = -9.81f;
 
     [Header("Model")]
     public Transform RootGeometry;
 
     private Vector3 _targetDirection = Vector3.zero;
+    private float _targetVelocityXZ = 0f;
+    private float _targetVelocityChangeRate = 0f;
     private float _targetVelocityY = 0f;
     private float _terminalVelocityY = -53f;
     private bool _playerJumping = false;
     private float _playerJumpTimer = 0f;
+    private float _playerCoyoteTimer = 0f;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -41,48 +47,16 @@ public class BasicPlayerController : MonoBehaviour
         MainCamera.transform.LookAt(CameraRoot);
         _originalCameraLocalPosition = MainCamera.transform.localPosition;
         _maxCameraDistance = Vector3.Distance(CameraRoot.transform.position, MainCamera.transform.position);
+
+        _targetVelocityChangeRate = 1 / TimeToMaxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        InputsCheck();
         CameraCheck();
-        GravityCheck();
+        JumpAndGravityCheck();
         MovePlayer();
-    }
-
-    void InputsCheck()
-    {
-        //Horizontal Movement
-        _targetDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
-        //Vertical Movement
-        if(_characterController.isGrounded)
-        {
-            
-            _playerJumping = false;
-            if (Input.GetKeyDown(KeyCode.Space) || _playerJumpTimer > 0)
-            {
-                _targetVelocityY = Mathf.Sqrt(JumpHeight * -2f * Gravity);
-                _playerJumping = true;
-            }
-            _playerJumpTimer = 0;
-        }
-        else
-        {
-            //Early jump stop
-            if(Input.GetKeyUp(KeyCode.Space) && _playerJumping && _targetVelocityY > 0)
-            {
-                _targetVelocityY *= 0.5f;
-            }
-
-            //Jump Leniency
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _playerJumpTimer = JumpLeniency;
-            }
-            _playerJumpTimer -= Time.deltaTime;
-        }
     }
 
     void CameraCheck()
@@ -111,10 +85,45 @@ public class BasicPlayerController : MonoBehaviour
         }
     }
 
-    void GravityCheck()
+    void JumpAndGravityCheck()
     {
-        if (!_characterController.isGrounded)
+        if (_characterController.isGrounded)
         {
+            _targetVelocityY = 0;
+            _playerJumping = false;
+            if (Input.GetKeyDown(KeyCode.Space) || _playerJumpTimer > 0)
+            {
+                _targetVelocityY = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                _playerJumping = true;
+            }
+            _playerJumpTimer = 0;
+            _playerCoyoteTimer = JumpLeniency;
+        }
+        else
+        {
+            //Coyote Time
+            //Early jump stop
+            if (Input.GetKeyUp(KeyCode.Space) && _playerJumping && _targetVelocityY > 0)
+            {
+                _targetVelocityY *= 0.5f;
+            }
+
+            //Jump Leniency
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_playerCoyoteTimer > 0)
+                {
+                    _targetVelocityY = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                    _playerJumping = true;
+                }
+                else
+                {
+                    _playerJumpTimer = JumpLeniency;
+                }
+            }
+            _playerCoyoteTimer -= Time.deltaTime;
+            _playerJumpTimer -= Time.deltaTime;
+
             _targetVelocityY += Gravity * Time.deltaTime;
             if (_targetVelocityY < _terminalVelocityY)
             {
@@ -125,12 +134,33 @@ public class BasicPlayerController : MonoBehaviour
 
     void MovePlayer()
     {
-        //Use CameraRoots rotation, making forward always in front of the camera
-        _targetDirection = Quaternion.Euler(0.0f, CameraRoot.rotation.eulerAngles.y, 0.0f) * _targetDirection;
-        //assign targetDirection to Geometry ?
-        RootGeometry.transform.LookAt(_characterController.transform.position + _targetDirection);
+        Vector3 newDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
 
-        _targetDirection *= MaxSpeed;
+        if(newDirection == Vector3.zero)
+        {
+            _targetVelocityXZ -= MaxSpeed * _targetVelocityChangeRate * Time.deltaTime; //10 * 1 * 0.5
+            if (_targetVelocityXZ < 0)
+            {
+                _targetVelocityXZ = 0;
+            }
+        }
+        else
+        {
+            _targetVelocityXZ += MaxSpeed * _targetVelocityChangeRate * Time.deltaTime;
+            if(_targetVelocityXZ > MaxSpeed)
+            {
+                _targetVelocityXZ = MaxSpeed;
+            }
+            //Use CameraRoots rotation, making forward always in front of the camera
+            _targetDirection = Quaternion.Euler(0.0f, CameraRoot.rotation.eulerAngles.y, 0.0f) * newDirection;
+            RootGeometry.transform.LookAt(_characterController.transform.position + _targetDirection);
+        }
+
+        //Keep the direction to magnitude 1
+        _targetDirection.y = 0;
+        _targetDirection.Normalize();
+        //Multiply by whatever currentVelocity we target
+        _targetDirection *= _targetVelocityXZ;
         _targetDirection.y = _targetVelocityY;
 
         _characterController.Move(_targetDirection * Time.deltaTime);
