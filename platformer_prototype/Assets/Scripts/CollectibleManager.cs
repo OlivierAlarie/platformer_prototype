6200a827b1704ee8dc95b7028003edcb341557c1
Charles-Antoine Solis <charles.a.solis@gmail.com>
2022-02-17 20:39:22 -0500

    new UI for collectibles

--- platformer_prototype/Assets/Scripts/CollectibleManager.cs
+++ platformer_prototype/Assets/Scripts/CollectibleManager.cs
@@ -7,10 +7,18 @@ using TMPro;
 public class CollectibleManager : MonoBehaviour
 {
     public TextMeshProUGUI rubisCount;
+    public TextMeshProUGUI keyShardCount;
+    public GameObject specialKeyShardImage;
     public KeyTracker keyTracker;
 
     void Update()
     {
         rubisCount.text = keyTracker.numberOfRubis.ToString("0");
+        keyShardCount.text = keyTracker._numberOfKeys.ToString("0");
+        if (keyTracker._hasSpecialKey) {
+            specialKeyShardImage.SetActive(true);
+        } else {
+            specialKeyShardImage.SetActive(false);
+        }
     }
 }
