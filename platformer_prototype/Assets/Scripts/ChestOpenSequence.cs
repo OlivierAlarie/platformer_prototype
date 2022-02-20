ba333a5e8ff8a548e6738c6cf8731cb5b3fa4df3
Charles-Antoine Solis <charles.a.solis@gmail.com>
2022-02-19 22:43:41 -0500

    endsequence update

--- platformer_prototype/Assets/Scripts/ChestOpenSequence.cs
+++ platformer_prototype/Assets/Scripts/ChestOpenSequence.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestOpenSequence : MonoBehaviour
 {
     public KeyTracker keyTracker;
-    public Camera mainCam;
-    public Camera endSequenceCam;
     public Animator chestAnimator;
     public float numberOfKeysNeeded = 4f;
-    
+    public GameObject timerBar;
+    public GameObject player;
+
+    [Header("Cameras")]
+    public GameObject mainCam;
+    public GameObject newParent;
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player" && keyTracker._numberOfKeys == numberOfKeysNeeded && !keyTracker._hasSpecialKey) {
-            mainCam.enabled = false;
-            endSequenceCam.enabled = true;
             chestAnimator.SetBool("hasFullKey", true);
+            timerBar.SetActive(false);
+            mainCam.transform.parent = newParent.transform;
+            mainCam.transform.localPosition = Vector3.zero;
+            player.SetActive(false);
         }
         if (other.gameObject.tag == "Player" && keyTracker._numberOfKeys == numberOfKeysNeeded && keyTracker._hasSpecialKey) {
-            //mainCam.enabled = false;
-            endSequenceCam.enabled = true;
             chestAnimator.SetBool("hasSpecialKey", true);
+            timerBar.SetActive(false);
+            mainCam.transform.parent = newParent.transform;
+            mainCam.transform.localPosition = Vector3.zero;
+            player.SetActive(false);
         }
     }
 }
