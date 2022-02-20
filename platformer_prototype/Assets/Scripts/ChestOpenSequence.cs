a99f20f85173dbc4fb62a299b00c5f865567e7c4
Benleking <benoitgirardbernier@gmail.com>
2022-02-20 15:06:59 -0500

    ending sequence fait

--- platformer_prototype/Assets/Scripts/ChestOpenSequence.cs
+++ platformer_prototype/Assets/Scripts/ChestOpenSequence.cs
@@ -13,6 +13,8 @@ public class ChestOpenSequence : MonoBehaviour
     public GameObject player;
     public AudioSource chestsound;
     public TextMeshProUGUI textdefin;
+    public GameObject lepanneau;
+    private float timer = -1f;
 
     [Header("Cameras")]
     public GameObject mainCam;
@@ -20,7 +22,24 @@ public class ChestOpenSequence : MonoBehaviour
 
     private void Start()
     {
-        textdefin.text = "Congratulation on finishing the game 100%\n\nThere are no more secrets for you to find.\n\nExcept MAYBE the little green guy";
+        textdefin.enabled = false;
+        lepanneau.SetActive(false);
+    }
+    private void Update()
+    {
+        if (timer > -1)
+        {
+        if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                textdefin.enabled = true;
+                lepanneau.SetActive(true);
+            }
+            
+        }
     }
 
 
@@ -35,12 +54,13 @@ public class ChestOpenSequence : MonoBehaviour
             player.SetActive(false);
             if (keyTracker.numberOfRubis == 100)
             {
-                textdefin.text = "Congratulation on finishing the game 100%\n\nThere are no more secrets for you to find.\n\nExcept MAYBE the little green guy";
+                textdefin.text = "Congratulation on finishing the game 100%\n\nThere are no more secrets for you to find.\n\nExcept MAYBE the little green guy\n\nOn Behalf of Etienne, Charles-Antoine, Olivier and Benoit,\n Thank you for playing our game!";
             }
             if (keyTracker.numberOfRubis < 100)
             {
-                textdefin.text = "Congratulation on finishing the game\n\nand finding the hidden key now all that's left";
+                textdefin.text = "Congratulation on finishing the game\n\nand finding the hidden key!!\n\n Now if you wish to complete the game 100%,\n you can replay and try to find the " + (100-keyTracker.numberOfRubis) + " Gems that are still missing\n\nOn Behalf of Etienne, Charles-Antoine, Olivier and Benoit,\n Thank you for playing our game!";
             }
+            timer = 4f;
         }
         if (other.gameObject.tag == "Player" && keyTracker._numberOfKeys == numberOfKeysNeeded && !keyTracker._hasSpecialKey)
         {
@@ -50,7 +70,9 @@ public class ChestOpenSequence : MonoBehaviour
             mainCam.transform.parent = newParent.transform;
             mainCam.transform.localPosition = Vector3.zero;
             mainCam.transform.localRotation = Quaternion.identity;
+            textdefin.text = "Congratulation on finishing the game\n\n Now if you wish to complete the game 100%, and open the little chest,\n you can replay and try to to find the " + (100 - keyTracker.numberOfRubis) + " Gems that are still missing\nAs well as the hidden key\n\nOn Behalf of Etienne, Charles-Antoine, Olivier and Benoit,\n Thank you for playing our game!";
             player.SetActive(false);
+            timer = 4f;
         }
     }
 }
